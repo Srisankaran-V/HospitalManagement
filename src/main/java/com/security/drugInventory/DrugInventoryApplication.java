34ac8c0ac269ab6d152f03007100d28c64adfe3f
srisankaran <srisankaran.333@gmail.com>
2024-12-24 15:03:12 +0530

    JWT token working well

--- src/main/java/com/security/drugInventory/DrugInventoryApplication.java
+++ src/main/java/com/security/drugInventory/DrugInventoryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DrugInventoryApplication {
+public class  DrugInventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DrugInventoryApplication.class, args);
